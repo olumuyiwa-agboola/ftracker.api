21f531f9c8636c056fc2ca103fbe8b7f04b90f98
olumuyiwa-agboola <agboolaod@gmail.com>
2024-12-23 11:16:29 +0100

    Set up infrastructure and implement CRUD operations on transactions

---
+++ src/FinanceTrackerAPI.Core/Abstractions/IConnectionFactories/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace FinanceTrackerAPI.Core.Abstractions.IConnectionFactories
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection CreateTransactionsDbConnection();
+    }
+}
---
+++ src/FinanceTrackerAPI.Core/Abstractions/IRepositories/ITransactionsRepository.cs
@@ -0,0 +1,16 @@
+﻿using FinanceTrackerAPI.Core.Models;
+
+namespace FinanceTrackerAPI.Core.Abstractions.IRepositories;
+
+public interface ITransactionsRepository
+{
+    Task<List<Transaction>?> GetAllTransactions();
+
+    Task<int> DeleteTransaction(string transactionId);
+
+    Task<int> AddTransaction(Transaction transaction);
+
+    Task<int> UpdateTransaction(Transaction transaction);
+
+    Task<Transaction?> GetTransaction(string transactionId);
+}
