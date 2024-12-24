75513cdbae6bf5329ff6c4708ce97f4634bc9988
olumuyiwa-agboola <agboolaod@gmail.com>
2024-12-24 14:25:48 +0100

    Refactor domain service result handling

--- src/FinanceTrackerAPI.Web/APIs/TransactionAPIs.cs
+++ src/FinanceTrackerAPI.Web/APIs/TransactionAPIs.cs
@@ -13,84 +13,48 @@ namespace FinanceTrackerAPI.Web.APIs
                 .WithTags("Transactions")
                 .WithDescription("APIs for managing transactions.");
 
-            transactions.MapGet("/", async (ITransactionsService service) =>
-            {
-                var result = await service.GetAllTransactions();
+            transactions.MapGet("/", async (ITransactionsService service) => HandleResult(await service.GetAllTransactions()))
+                .WithName("GetAllTransactions")
+                .WithSummary("Get all transactions")
+                .WithDescription("Retrieves the details of all transactions")
+                .Produces<ProblemDetails>((int)HttpStatusCode.InternalServerError)
+                .Produces<List<Transaction>>((int)HttpStatusCode.OK);
 
-                return result.IsSuccess switch
-                {
-                    true => Results.Ok(result.Value),
-                    false => Results.Problem(statusCode: result.Problem!.Status, title: result.Problem.Title)
-                };
+            transactions.MapGet("/{id}", async ([FromRoute] string id, ITransactionsService service) => HandleResult(await service.GetTransaction(id)))
+                .WithName("GetTransactionById")
+                .WithSummary("Get a transaction")
+                .WithDescription("Retrieves the details of a particualar transaction")
+                .Produces<ProblemDetails>((int)HttpStatusCode.InternalServerError)
+                .Produces<ProblemDetails>((int)HttpStatusCode.NotFound)
+                .Produces<Transaction>((int)HttpStatusCode.OK);
 
-            }).WithName("GetAllTransactions")
-            .WithSummary("Get all transactions")
-            .WithDescription("Retrieves the details of all transactions")
-            .Produces<ProblemDetails>((int)HttpStatusCode.InternalServerError)
-            .Produces<List<Transaction>>((int)HttpStatusCode.OK);
+            transactions.MapPost("/", async ([FromBody] CreateTransactionRequest request, ITransactionsService service) => HandleResult(await service.CreateTransaction(request)))
+                .WithName("CreateTransaction")
+                .WithSummary("Create a transaction")
+                .WithDescription("Creates a new transaction")
+                .Produces<ProblemDetails>((int)HttpStatusCode.UnprocessableEntity)
+                .Produces<CreateTransactionResponse>((int)HttpStatusCode.OK);
 
-            transactions.MapGet("/{id}", async ([FromRoute] string id, ITransactionsService service) =>
-            {
-                var result = await service.GetTransaction(id);
+            transactions.MapPut("/{id}", async ([FromRoute] string id, UpdateTransactionRequest request, ITransactionsService service) => HandleResult(await service.UpdateTransaction(request)))
+                .WithName("UpdateTransaction")
+                .WithSummary("Update a transaction")
+                .WithDescription("Updates one or more of the fields of a transaction");
 
-                return result.IsSuccess switch
-                {
-                    true => Results.Ok(result.Value),
-                    false => Results.Problem(statusCode: result.Problem!.Status, title: result.Problem.Title)
-                };
-
-            }).WithName("GetTransactionById")
-            .WithSummary("Get a transaction")
-            .WithDescription("Retrieves the details of a particualar transaction")
-            .Produces<ProblemDetails>((int)HttpStatusCode.InternalServerError)
-            .Produces<ProblemDetails>((int)HttpStatusCode.NotFound)
-            .Produces<Transaction>((int)HttpStatusCode.OK);
-
-            transactions.MapPost("/", async ([FromBody] CreateTransactionRequest request, ITransactionsService service) =>
-            {
-                var result = await service.CreateTransaction(request);
-
-                return result.IsSuccess switch
-                {
-                    true => Results.Ok(result.Value),
-                    false => Results.Problem(statusCode: result.Problem!.Status, title: result.Problem.Title)
-                };
-
-            }).WithName("CreateTransaction")
-            .WithSummary("Create a transaction")
-            .WithDescription("Creates a new transaction")
-            .Produces<ProblemDetails>((int)HttpStatusCode.UnprocessableEntity)
-            .Produces<CreateTransactionResponse>((int)HttpStatusCode.OK);
-
-            transactions.MapPut("/{id}", async ([FromRoute] string id, UpdateTransactionRequest request, ITransactionsService service) =>
-            {
-                var result = await service.UpdateTransaction(request);
-
-                return result.IsSuccess switch
-                {
-                    true => Results.Ok(result.Value),
-                    false => Results.Problem(statusCode: result.Problem!.Status, title: result.Problem.Title)
-                };
-
-            }).WithName("UpdateTransaction")
-            .WithSummary("Update a transaction")
-            .WithDescription("Updates one or more of the fields of a transaction");
-
-            transactions.MapDelete("/{id}", async ([FromRoute] string id, ITransactionsService service) =>
-            {
-                var result = await service.DeleteTransaction(id);
-
-                return result.IsSuccess switch
-                {
-                    true => Results.Ok(result.Value),
-                    false => Results.Problem(statusCode: result.Problem!.Status, title: result.Problem.Title)
-                };
-
-            }).WithName("DeleteTransaction")
-            .WithSummary("Delete a transaction")
-            .WithDescription("Deletes a transaction");
+            transactions.MapDelete("/{id}", async ([FromRoute] string id, ITransactionsService service) => HandleResult(await service.DeleteTransaction(id)))
+                .WithName("DeleteTransaction")
+                .WithSummary("Delete a transaction")
+                .WithDescription("Deletes a transaction");
 
             return app;
+        }
+
+        public static IResult HandleResult<TResult>(TResult result) where TResult : Core.Result.IResult
+        {
+            return result.IsSuccess switch
+            {
+                true => Results.Ok(result.GetValue()),
+                false => Results.Problem(statusCode: result.Problem!.Status, title: result.Problem.Title)
+            };
         }
     }
 }
